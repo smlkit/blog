2d937851719d50e0b5b7bf918a76a80f8ec7cbee
websailors <websailors@websailorss-iMac.local>
2023-04-27 12:34:11 +0300

    add icons

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,28 +1,38 @@
 import React from "react";
-import { Typography, Button, Stack } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Wrapper } from "../../containers/wrapper";
+import { Typography, Button, Stack } from "@mui/material";
+import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
 const HomePage = () => {
   const navigate = useNavigate();
 
   return (
     <Wrapper>
-      <Stack textAlign="center">
-        <Typography variant="h4" color="text.primary" gutterBottom>
-          Welcome!
-        </Typography>
-        <Typography variant="body1" color="text.secondary">
-          Please choose what you want to do:
-        </Typography>
-      </Stack>
-      <Stack direction="row" spacing={2}>
-        <Button variant="contained" size="large" onClick={() => navigate(`/posts`)}>
-          Browse posts
-        </Button>
-        <Button variant="contained" size="large" onClick={() => navigate(`/albums`)}>
-          Browse albums
-        </Button>
+      <Stack gap="40px" paddingTop="150px">
+        <Stack textAlign="center">
+          <Typography variant="h4" color="text.primary" gutterBottom>
+            Welcome!
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            Please choose what you want to do:
+          </Typography>
+        </Stack>
+        <Stack direction="row" spacing={2}>
+          <Button variant="contained" size="large" onClick={() => navigate(`/posts`)}>
+            <Stack direction="row" gap="5px" alignItems="center">
+              <DescriptionOutlinedIcon fontSize="small" />
+              Browse posts
+            </Stack>
+          </Button>
+          <Button variant="contained" size="large" onClick={() => navigate(`/albums`)}>
+            <Stack direction="row" gap="5px" alignItems="center">
+              <CameraAltOutlinedIcon fontSize="small" />
+              Browse albums
+            </Stack>
+          </Button>
+        </Stack>
       </Stack>
     </Wrapper>
   );
